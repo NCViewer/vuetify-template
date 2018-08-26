81c5e46076447cc163bb11fbff9c4a0ac1f7258e
Xander Luciano <xanderluciano@gmail.com>
2018-08-26 11:16:32 -0700

    Create left menu module

---
+++ template/components/LeftMenu.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-navigation-drawer
+        :mini-variant.sync="miniMenu"
+        :clipped="clipMenu"
+        :value = "showLeftMenu"
+        @input="updateLeftMenu"
+        fixed app>
+        <v-list>
+            <v-list-tile
+                :to="item.to"
+                v-for="(item, i) in items"
+                :key="i"
+                router exact>
+
+                <v-list-tile-action>
+                    <v-icon v-html="item.icon" />
+                </v-list-tile-action>
+
+                <v-list-tile-content>
+                    <v-list-tile-title v-text="item.title" />
+                </v-list-tile-content>
+
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    import { mapMutations, mapState } from 'vuex'
+
+    export default {
+        name: 'LeftMenu',
+
+        data() {
+            return {
+                items: [
+                    { icon: 'apps', title: 'Welcome', to: '/' },
+                    { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
+                ],
+            }
+        },
+
+        methods: {
+            ...mapMutations({
+                setLeftMenu: 'SET_showLeftMenu',
+            }),
+            updateLeftMenu (state) { if (!state) this.setLeftMenu(false) },
+        },
+
+        computed: {
+            ...mapState({
+                showLeftMenu: 'showLeftMenu',
+                miniMenu: 'miniMenu',
+                clipMenu: 'clipMenu',
+            }),
+        },
+    }
+</script>
--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -2,28 +2,7 @@
     <v-app :dark="dark">
 
         <!-- Left Nav -->
-        <v-navigation-drawer
-            :mini-variant.sync="miniMenu"
-            :clipped="clipMenu"
-            :value = "showLeftMenu"
-            @input="updateLeftMenu"
-            fixed app>
-            <v-list>
-                <v-list-tile
-                    router
-                    :to="item.to"
-                    :key="i"
-                    v-for="(item, i) in items"
-                    exact>
-                    <v-list-tile-action>
-                        <v-icon v-html="item.icon" />
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title v-text="item.title" />
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
+        <left-menu />
 
         <!-- Toolbar -->
         <toolbar :title="title" />
@@ -64,19 +43,17 @@
 <script>
     import { mapState, mapMutations } from 'vuex'
     import toolbar from '~/components/toolbar'
+    import LeftMenu from '~/components/LeftMenu'
 
     export default {
         components: {
             toolbar,
+            LeftMenu,
         },
 
         data () {
             return {
                 title: 'Template',
-                items: [
-                    { icon: 'apps', title: 'Welcome', to: '/' },
-                    { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
-                ],
                 right: true,
             }
         },
@@ -84,16 +61,13 @@
         methods: {
             ...mapMutations({
                 setRightMenu: 'SET_showRightMenu',
-                setLeftMenu: 'SET_showLeftMenu',
             }),
-            updateLeftMenu (state) { if (!state) this.setLeftMenu(false) },
             updateRightMenu (state) { if (!state) this.setRightMenu(false) },
         },
 
         computed: {
             ...mapState({
                 dark: 'dark',
-                showLeftMenu: 'showLeftMenu',
                 showRightMenu: 'showRightMenu',
                 miniMenu: 'miniMenu',
                 clipMenu: 'clipMenu',
