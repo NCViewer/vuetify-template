eef0a6ab675228ae0d436a7aac229f422a1ce245
Xander Luciano <xanderluciano@gmail.com>
2018-08-26 11:32:05 -0700

    Create right menu module

---
+++ template/components/RightMenu.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-navigation-drawer
+        :right="right"
+        :value="showRightMenu"
+        @input="updateRightMenu"
+        fixed temporary>
+        <v-list>
+            <v-list-tile @click.native="right = !right">
+
+                <v-list-tile-action>
+                    <v-icon light>compare_arrows</v-icon>
+                </v-list-tile-action>
+
+                <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    import { mapMutations, mapState } from 'vuex'
+
+    export default {
+        name: 'RightMenu',
+
+        data() {
+            return {
+                right: true,
+            }
+        },
+
+        methods: {
+            ...mapMutations({
+                setRightMenu: 'SET_showRightMenu',
+            }),
+            updateRightMenu (state) { if (!state) this.setRightMenu(false) },
+        },
+
+        computed: {
+            ...mapState({
+                showRightMenu: 'showRightMenu',
+                clipMenu: 'clipMenu',
+            }),
+        },
+    }
+</script>
--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -15,26 +15,12 @@
         </v-content>
 
         <!-- Right Nav -->
-        <v-navigation-drawer
-            :right="right"
-            :value="showRightMenu"
-            @input="updateRightMenu"
-            fixed temporary>
-            <v-list>
-                <v-list-tile @click.native="right = !right">
-                    <v-list-tile-action>
-                        <v-icon light>compare_arrows</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
+        <right-menu />
 
         <!-- Footer -->
         <v-footer :fixed="fixedFooter" app>
             <v-flex px-3>
                 <span v-text="`© ${ new Date().getFullYear() }`" />
-            </v-flex>
         </v-footer>
 
     </v-app>
@@ -42,35 +28,31 @@
 
 <script>
     import { mapState, mapMutations } from 'vuex'
+
     import toolbar from '~/components/toolbar'
     import LeftMenu from '~/components/LeftMenu'
+    import RightMenu from '~/components/RightMenu'
 
     export default {
         components: {
             toolbar,
             LeftMenu,
+            RightMenu,
         },
 
         data () {
             return {
                 title: 'Template',
-                right: true,
             }
         },
 
         methods: {
-            ...mapMutations({
-                setRightMenu: 'SET_showRightMenu',
-            }),
-            updateRightMenu (state) { if (!state) this.setRightMenu(false) },
+
         },
 
         computed: {
             ...mapState({
                 dark: 'dark',
-                showRightMenu: 'showRightMenu',
-                miniMenu: 'miniMenu',
-                clipMenu: 'clipMenu',
                 fixedFooter: 'fixedFooter',
             }),
         },
